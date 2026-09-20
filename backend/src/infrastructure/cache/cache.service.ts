305791774f0dcf4ae920b6a704706f81492cac57
Md Ikbal <ikbal84336@gmail.com>
2026-09-20 19:24:06 +0530

    Fixed the return in the increament method in the cache.service.ts

--- backend/src/infrastructure/cache/cache.service.ts
+++ backend/src/infrastructure/cache/cache.service.ts
@@ -57,7 +57,7 @@ export class CacheService {
   }
   async increament(key: string): Promise<number> {
     try {
-      await redis.incr(key);
+      return await redis.incr(key);
     } catch (error) {
       logger.error({ error, key }, "Failed to increament cache key");
       return 0;
